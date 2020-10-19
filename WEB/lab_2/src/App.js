7d08c616d1ea0ce3d0c0b2cc1e95d95d7c1d9a31
C0d1red <bogomolov0602@mail.ru>
2020-10-20 00:31:26 +0300

    init

---
+++ WEB/lab_2/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>
+          Made by Bogomolov R.D.
+        </p>
+        <p>
+          IKBO-04-17
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
